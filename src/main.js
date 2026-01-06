b590af76e17b5ad1ae3081c13e56f26279acf3ab
risou <153372819+ris0u@users.noreply.github.com>
2026-01-06 14:39:08 +0800

    v0.1

--- src/main.js
+++ src/main.js
@@ -1,8 +1,39 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
+<<<<<<< Updated upstream
 
 const app = createApp(App);
 
 
 app.mount('#app');
+=======
+// Vue Router
+import { createRouter, createWebHistory } from 'vue-router'
+import LoginPage from './components/login.vue'
+import RegisterPage from './components/register.vue'
+import Homepage from './components/home.vue'
+
+// Routes
+const routes = [
+    { path: '/', redirect: '/login' },
+    { path: '/login', name: 'Login', component: LoginPage },
+    { path: '/register', name: 'Register', component: RegisterPage },
+    { path: '/home', name: 'Home', component: Homepage },
+]
+
+// Router instance
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+// Create app
+const app = createApp(App)
+app.use(createPinia())
+app.use(router)       // <- must be before mount
+app.mount("#app")
+>>>>>>> Stashed changes
